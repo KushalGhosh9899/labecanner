e386bace700fb94c13bf2ba9079f54d688708b1b
Kushal Ghosh <Kushalghosh9899@gmail.com>
2025-12-28 15:34:02 +0530

    update(config): Gemini API key and django secret

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!rj00@fwi!yxi67kl9_9sfd71e-m39w7hvo0cdlo6aqthz!vny'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
